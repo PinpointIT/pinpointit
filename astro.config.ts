ca046234c7c87daa1251d099f4b589493d90b2fa
David Dankelmann <mail@david-dankelmann.de>
2025-12-26 11:32:26 +0100

    Add deployment to Github Pages

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://pinpointit.github.io',
+  base: '/pinpointit',
 
   integrations: [
     tailwind({
